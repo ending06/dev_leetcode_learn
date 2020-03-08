cd652dec7f504952b2f4535362c69adeb81ddeb4
ruirui.qu <ruirui.qu@qunar.com>
2020-03-08 14:56:32 +0800

    init

---
+++ src/main/java/com/leetcode/code38/InitMain.java
@@ -0,0 +1,12 @@
+package com.leetcode.code38;
+
+//--------------------- Change Logs----------------------
+// <p>@author ruirui.qu Initial Created at 2020-03-08<p>
+//-------------------------------------------------------
+
+public class InitMain {
+
+    public static void main(String args[]){
+        System.out.println("==>init初始化");
+    }
+}
---
+++ src/test/java/com/leetcode/codetest/AppTest.java
@@ -0,0 +1,9 @@
+package com.leetcode.codetest;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+}
